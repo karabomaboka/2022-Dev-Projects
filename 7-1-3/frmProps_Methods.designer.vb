5779fffd2c504c41327763773aa7789332c7a37a
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-01-07 22:34:56 +0200

    Add files via upload

---
+++ 7-1-3/frmProps_Methods.designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmProps_Methods
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Me.btnDisplay = New System.Windows.Forms.Button()
+    Me.lstValues = New System.Windows.Forms.ListBox()
+    Me.SuspendLayout()
+    '
+    'btnDisplay
+    '
+    Me.btnDisplay.Location = New System.Drawing.Point(124, 12)
+    Me.btnDisplay.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+    Me.btnDisplay.Name = "btnDisplay"
+    Me.btnDisplay.Size = New System.Drawing.Size(249, 36)
+    Me.btnDisplay.TabIndex = 0
+    Me.btnDisplay.Text = "Display Values"
+    Me.btnDisplay.UseVisualStyleBackColor = True
+    '
+    'lstValues
+    '
+    Me.lstValues.FormattingEnabled = True
+    Me.lstValues.ItemHeight = 16
+    Me.lstValues.Location = New System.Drawing.Point(124, 55)
+    Me.lstValues.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+    Me.lstValues.Name = "lstValues"
+    Me.lstValues.Size = New System.Drawing.Size(248, 116)
+    Me.lstValues.TabIndex = 1
+    '
+    'frmProps_Methods
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(496, 185)
+    Me.Controls.Add(Me.lstValues)
+    Me.Controls.Add(Me.btnDisplay)
+    Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+    Me.Name = "frmProps_Methods"
+    Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+    Me.Text = "Presidential Ages at Inauguration"
+    Me.ResumeLayout(False)
+
+  End Sub
+  Friend WithEvents btnDisplay As System.Windows.Forms.Button
+  Friend WithEvents lstValues As System.Windows.Forms.ListBox
+
+End Class
---
+++ 7-1-3/frmProps_Methods.vb
@@ -0,0 +1,18 @@
+﻿Public Class frmProps_Methods
+
+  Private Sub btnDisplay_Click() Handles btnDisplay.Click
+    Dim ages(44) As Integer
+    Dim temp() As String = IO.File.ReadAllLines("AgesAtInaugural.txt")
+    For i As Integer = 0 To 44
+      ages(i) = CInt(temp(i))
+    Next
+    lstValues.Items.Add("Trump: " & ages(ages.Count - 1))
+    lstValues.Items.Add("Washington: " & ages.First)
+    lstValues.Items.Add("Trump: " & ages.Last)
+    lstValues.Items.Add("Oldest age: " & ages.Max)
+    lstValues.Items.Add("Youngest age: " & ages.Min)
+    lstValues.Items.Add("Average age: " & (ages.Average).ToString("N"))
+    lstValues.Items.Add("Average age: " & (ages.Sum / ages.Count).ToString("N"))
+  End Sub
+
+End Class
