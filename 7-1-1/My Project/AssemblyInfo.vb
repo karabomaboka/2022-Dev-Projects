1253560aa64cde8655a36729cb6009ac840c481c
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-01-07 22:33:01 +0200

    Add files via upload

---
+++ 7-1-1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("7-1-1")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("7-1-1")> 
+<Assembly: AssemblyCopyright("Copyright ©  2009")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("76989f97-76ef-4f37-bf0f-f23327e2e2f9")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ 7-1-1/frmBowl.vb
@@ -0,0 +1,19 @@
+﻿Public Class frmBowl
+
+  Private Sub btnWhoWon_Click(sender As Object, e As EventArgs) Handles btnWhoWon.Click
+    Dim teamNames(3) As String
+    Dim n As Integer
+    'Place Super Bowl Winners into the array
+    teamNames(0) = "Packers"
+    teamNames(1) = "Packers"
+    teamNames(2) = "Jets"
+
+    teamNames(3) = "Chiefs"
+    'Access array
+    n = CInt(mtbNumber.Text)
+    txtWinner.Text = teamNames(n - 1)
+  End Sub
+
+End Class
+
+
