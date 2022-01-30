2ea0b3bede131e217fc474618f7005edf68e0b1e
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-01-30 15:40:34 +0200

    Add files via upload

---
+++ 5-3-1/frmDemo.designer.vb
@@ -0,0 +1,69 @@
+﻿Partial Public Class frmDemo
+  Inherits System.Windows.Forms.Form
+
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Public Sub New()
+    MyBase.New()
+
+    'This call is required by the Windows Form Designer.
+    InitializeComponent()
+
+  End Sub
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+    If disposing AndAlso components IsNot Nothing Then
+      components.Dispose()
+    End If
+    MyBase.Dispose(disposing)
+  End Sub
+
+  Friend WithEvents btnDisplay As System.Windows.Forms.Button
+  Friend WithEvents lstResults As System.Windows.Forms.ListBox
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.Container
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
+    Me.lstResults = New System.Windows.Forms.ListBox()
+    Me.btnDisplay = New System.Windows.Forms.Button()
+    Me.SuspendLayout()
+    '
+    'lstResults
+    '
+    Me.lstResults.ItemHeight = 16
+    Me.lstResults.Location = New System.Drawing.Point(46, 59)
+    Me.lstResults.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.lstResults.Name = "lstResults"
+    Me.lstResults.Size = New System.Drawing.Size(189, 84)
+    Me.lstResults.TabIndex = 1
+    '
+    'btnDisplay
+    '
+    Me.btnDisplay.Location = New System.Drawing.Point(45, 10)
+    Me.btnDisplay.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.btnDisplay.Name = "btnDisplay"
+    Me.btnDisplay.Size = New System.Drawing.Size(192, 36)
+    Me.btnDisplay.TabIndex = 0
+    Me.btnDisplay.Text = "Display Results"
+    '
+    'frmDemo
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(282, 154)
+    Me.Controls.Add(Me.lstResults)
+    Me.Controls.Add(Me.btnDisplay)
+    Me.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.Name = "frmDemo"
+    Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+    Me.Text = "5-3-1"
+    Me.ResumeLayout(False)
+
+  End Sub
+
+End Class
---
+++ 5-3-1/frmDemo.vb
@@ -0,0 +1,19 @@
+﻿Public Class frmDemo
+
+  Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
+    'Illustrate that a change in value of parameter does not alter the
+    'value of the argument
+    Dim amt As Double = 2
+    lstResults.Items.Add(amt & " from event procedure")
+    Triple(amt)
+    lstResults.Items.Add(amt & " from event procedure")
+  End Sub
+
+  Sub Triple(num As Double)
+    'Triple a number
+    lstResults.Items.Add(num & " from Sub procedure")
+    num = 3 * num
+    lstResults.Items.Add(num & " from Sub procedure")
+  End Sub
+
+End Class
