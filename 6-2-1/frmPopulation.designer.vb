b5b0a182dec63f3cef910a6da9460f581bb03469
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-02-04 18:44:39 +0200

    Add files via upload

---
+++ 6-2-1/frmPopulation.designer.vb
@@ -0,0 +1,70 @@
+﻿Partial Public Class frmPopulation
+  Inherits System.Windows.Forms.Form
+
+  <System.Diagnostics.DebuggerNonUserCode()>
+  Public Sub New()
+    MyBase.New()
+
+    'This call is required by the Windows Form Designer.
+    InitializeComponent()
+
+  End Sub
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()>
+  Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+    If disposing AndAlso components IsNot Nothing Then
+      components.Dispose()
+    End If
+    MyBase.Dispose(disposing)
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()>
+  Private Sub InitializeComponent()
+    Me.btnDisplay = New System.Windows.Forms.Button()
+    Me.lstTable = New System.Windows.Forms.ListBox()
+    Me.SuspendLayout()
+    '
+    'btnDisplay
+    '
+    Me.btnDisplay.Location = New System.Drawing.Point(74, 9)
+    Me.btnDisplay.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.btnDisplay.Name = "btnDisplay"
+    Me.btnDisplay.Size = New System.Drawing.Size(158, 36)
+    Me.btnDisplay.TabIndex = 0
+    Me.btnDisplay.Text = "Display Population"
+    '
+    'lstTable
+    '
+    Me.lstTable.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.lstTable.FormattingEnabled = True
+    Me.lstTable.ItemHeight = 17
+    Me.lstTable.Location = New System.Drawing.Point(74, 57)
+    Me.lstTable.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.lstTable.Name = "lstTable"
+    Me.lstTable.Size = New System.Drawing.Size(158, 89)
+    Me.lstTable.TabIndex = 1
+    '
+    'frmPopulation
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(307, 160)
+    Me.Controls.Add(Me.lstTable)
+    Me.Controls.Add(Me.btnDisplay)
+    Me.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.Name = "frmPopulation"
+    Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+    Me.Text = "Population"
+    Me.ResumeLayout(False)
+
+  End Sub
+  Friend WithEvents btnDisplay As System.Windows.Forms.Button
+  Friend WithEvents lstTable As System.Windows.Forms.ListBox
+End Class
---
+++ 6-2-1/frmPopulation.vb
@@ -0,0 +1,12 @@
+﻿Public Class frmPopulation
+
+  Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
+    'Display population from 2018 to 2022
+    Dim pop As Double = 300000
+    For yr As Integer = 2018 To 2022
+      lstTable.Items.Add(yr & "     " & pop.ToString("N0"))
+      pop += 0.03 * pop
+    Next
+  End Sub
+
+End Class
