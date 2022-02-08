1ae73f22f55879508a1e538e8bd75f6ef5c8ac49
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-02-08 21:57:21 +0200

    Add files via upload

---
+++ 6-2-intro1/frmTable.vb
@@ -0,0 +1,11 @@
+﻿Public Class frmTable
+
+  Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
+    'Display a table of the first 5 numbers and their squares
+    'Assume the font for lstTable is Courier New
+    For i As Integer = 1 To 5
+      lstTable.Items.Add(i & "  " & i ^ 2)
+    Next
+  End Sub
+
+End Class
