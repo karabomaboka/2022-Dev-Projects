4b6f5a64a94a58dcb4cc3684e133e787591abf16
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-02-09 09:46:17 +0200

    Add files via upload

---
+++ 6-2-intro2/frmTable.designer.vb
@@ -0,0 +1,71 @@
+﻿Partial Public Class frmTable
+  Inherits System.Windows.Forms.Form
+
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Public Sub New()
+    MyBase.New()
+
+    'This call is required by the Windows Form Designer.
+    InitializeComponent()
+
+  End Sub
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+    If disposing AndAlso components IsNot Nothing Then
+      components.Dispose()
+    End If
+    MyBase.Dispose(disposing)
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Me.btnDisplay = New System.Windows.Forms.Button()
+    Me.lstTable = New System.Windows.Forms.ListBox()
+    Me.SuspendLayout()
+    '
+    'btnDisplay
+    '
+    Me.btnDisplay.Location = New System.Drawing.Point(53, 16)
+    Me.btnDisplay.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.btnDisplay.Name = "btnDisplay"
+    Me.btnDisplay.Size = New System.Drawing.Size(200, 36)
+    Me.btnDisplay.TabIndex = 0
+    Me.btnDisplay.Text = "Display Table"
+    '
+    'lstTable
+    '
+    Me.lstTable.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.lstTable.FormattingEnabled = True
+    Me.lstTable.ItemHeight = 17
+    Me.lstTable.Location = New System.Drawing.Point(54, 68)
+    Me.lstTable.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.lstTable.Name = "lstTable"
+    Me.lstTable.Size = New System.Drawing.Size(197, 106)
+    Me.lstTable.TabIndex = 1
+    '
+    'frmTable
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(307, 185)
+    Me.Controls.Add(Me.lstTable)
+    Me.Controls.Add(Me.btnDisplay)
+    Me.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.Name = "frmTable"
+    Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+    Me.Text = "6-2-Intro2"
+    Me.ResumeLayout(False)
+
+  End Sub
+  Friend WithEvents btnDisplay As System.Windows.Forms.Button
+  Friend WithEvents lstTable As System.Windows.Forms.ListBox
+
+End Class
---
+++ 6-2-intro2/frmTable.vb
@@ -0,0 +1,13 @@
+﻿Public Class frmTable
+
+  Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
+    'Display a table of the first 5 numbers and their squares
+    Dim i As Integer
+    i = 1
+    Do While i <= 5
+      lstTable.Items.Add(i & "  " & i ^ 2)
+      i += 1    'Add 1 to i
+    Loop
+  End Sub
+
+End Class
