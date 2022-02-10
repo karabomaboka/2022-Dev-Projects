cb954dbc9827ef1c3c5291dd38d237dd1ab9f7a4
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-02-10 17:44:11 +0200

    Add files via upload

---
+++ 6-2-4/frmTable.vb
@@ -0,0 +1,16 @@
+﻿Public Class frmTable
+
+  Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
+    Dim row, entry As String
+    lstTable.Items.Clear()
+    For j As Integer = 1 To 3
+      row = ""
+      For k As Integer = 1 To 3
+        entry = j & " x " & k & " = " & (j * k)
+        row &= entry & "    "
+      Next
+      lstTable.Items.Add(row)
+    Next
+  End Sub
+
+End Class
