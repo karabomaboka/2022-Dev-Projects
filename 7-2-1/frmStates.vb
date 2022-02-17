5155bbeb87fd0f9c486eb40cd08e77536e4284f0
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-02-17 17:07:30 +0200

    Add files via upload

---
+++ 7-2-1/frmStates.vb
@@ -0,0 +1,20 @@
+﻿Public Class frmStates
+
+  Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
+    Dim states() As String = IO.File.ReadAllLines("States.txt")
+    Dim stateQuery1 = From state As String In states
+                      Where state.Length = 5
+                      Select state
+    For Each state As String In stateQuery1
+      lstStates.Items.Add(state)
+    Next
+    lstStates.Items.Add("")
+    Dim stateQuery2 = From state As String In states
+                      Where state.StartsWith("New")
+                      Select state
+    For Each state In stateQuery2
+      lstStates.Items.Add(state)
+    Next
+  End Sub
+
+End Class
