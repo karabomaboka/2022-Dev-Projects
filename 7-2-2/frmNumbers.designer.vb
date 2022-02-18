5b96b1a573d4b64d6b732b177441ed17a899f08a
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-02-18 13:43:17 +0200

    Add files via upload

---
+++ 7-2-2/frmNumbers.designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmNumbers
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Me.btnDisplay = New System.Windows.Forms.Button()
+    Me.lstBox = New System.Windows.Forms.ListBox()
+    Me.SuspendLayout()
+    '
+    'btnDisplay
+    '
+    Me.btnDisplay.Location = New System.Drawing.Point(48, 11)
+    Me.btnDisplay.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+    Me.btnDisplay.Name = "btnDisplay"
+    Me.btnDisplay.Size = New System.Drawing.Size(207, 36)
+    Me.btnDisplay.TabIndex = 0
+    Me.btnDisplay.Text = "Display Numbers"
+    Me.btnDisplay.UseVisualStyleBackColor = True
+    '
+    'lstBox
+    '
+    Me.lstBox.FormattingEnabled = True
+    Me.lstBox.ItemHeight = 16
+    Me.lstBox.Location = New System.Drawing.Point(49, 57)
+    Me.lstBox.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+    Me.lstBox.Name = "lstBox"
+    Me.lstBox.Size = New System.Drawing.Size(204, 164)
+    Me.lstBox.TabIndex = 1
+    '
+    'frmNumbers
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(302, 228)
+    Me.Controls.Add(Me.lstBox)
+    Me.Controls.Add(Me.btnDisplay)
+    Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+    Me.Name = "frmNumbers"
+    Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+    Me.Text = "Numbers"
+    Me.ResumeLayout(False)
+
+  End Sub
+  Friend WithEvents btnDisplay As System.Windows.Forms.Button
+  Friend WithEvents lstBox As System.Windows.Forms.ListBox
+
+End Class
---
+++ 7-2-2/frmNumbers.vb
@@ -0,0 +1,23 @@
+﻿Public Class frmNumbers
+
+  Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
+    Dim nums() As Integer = {5, 12, 8, 7, 11}
+    Dim numQuery1 = From num In nums
+                    Where num > 7
+                    Select num
+    For Each num As Integer In numQuery1
+      lstBox.Items.Add(num)
+    Next
+    lstBox.Items.Add("Largest number: " & numQuery1.Max)
+    lstBox.Items.Add("Second number: " & numQuery1(1))
+    lstBox.Items.Add("Sum of numbers: " & numQuery1.Sum)
+    lstBox.Items.Add("")
+    Dim numQuery2 = From num In nums
+                    Where num Mod 2 = 0
+                    Select num
+    lstBox.Items.Add("Number of even numbers: " & numQuery2.Count)
+    lstBox.Items.Add("Average of even numbers: " & numQuery2.Average)
+    lstBox.Items.Add("Last even number: " & numQuery2.Last)
+  End Sub
+
+End Class
