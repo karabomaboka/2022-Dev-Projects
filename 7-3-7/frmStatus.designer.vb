3ed25c8849d1ffca2b8a25330f860c266fc321d7
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-03-02 17:19:50 +0200

    Add files via upload

---
+++ 7-3-7/frmStatus.designer.vb
@@ -0,0 +1,71 @@
+﻿Partial Public Class frmStatus
+  Inherits System.Windows.Forms.Form
+
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Public Sub New()
+    MyBase.New()
+
+    'This call is required by the Windows Form Designer.
+    InitializeComponent()
+
+  End Sub
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+    If disposing AndAlso components IsNot Nothing Then
+      components.Dispose()
+    End If
+    MyBase.Dispose(disposing)
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Me.btnGet = New System.Windows.Forms.Button()
+    Me.txtResult = New System.Windows.Forms.TextBox()
+    Me.SuspendLayout()
+    '
+    'btnGet
+    '
+    Me.btnGet.Location = New System.Drawing.Point(56, 10)
+    Me.btnGet.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.btnGet.Name = "btnGet"
+    Me.btnGet.Size = New System.Drawing.Size(244, 36)
+    Me.btnGet.TabIndex = 0
+    Me.btnGet.Text = "Get Data and Determine Status"
+    '
+    'txtResult
+    '
+    Me.txtResult.Location = New System.Drawing.Point(9, 54)
+    Me.txtResult.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.txtResult.Name = "txtResult"
+    Me.txtResult.ReadOnly = True
+    Me.txtResult.Size = New System.Drawing.Size(339, 22)
+    Me.txtResult.TabIndex = 1
+    Me.txtResult.Text = " "
+    '
+    'frmStatus
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(356, 90)
+    Me.Controls.Add(Me.txtResult)
+    Me.Controls.Add(Me.btnGet)
+    Me.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.Name = "frmStatus"
+    Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+    Me.Text = "Graduation Status"
+    Me.ResumeLayout(False)
+    Me.PerformLayout()
+
+  End Sub
+  Friend WithEvents btnGet As System.Windows.Forms.Button
+  Friend WithEvents txtResult As System.Windows.Forms.TextBox
+
+End Class
---
+++ 7-3-7/frmStatus.vb
@@ -0,0 +1,41 @@
+﻿Public Class frmStatus
+
+  Structure FullName
+    Dim firstName As String
+    Dim lastName As String
+  End Structure
+
+  Structure Student
+    Dim name As FullName
+    Dim credits() As Integer
+  End Structure
+
+  Private Sub btnGet_Click(sender As System.Object, e As System.EventArgs) Handles btnGet.Click
+    Dim numYears As Integer
+    Dim person As Student
+    txtResult.Clear()
+    person.name.firstName = InputBox("First Name:")
+    person.name.lastName = InputBox("Last Name:")
+    numYears = CInt(InputBox("Number of years completed:"))
+    ReDim person.credits(numYears - 1)
+    For i As Integer = 0 To numYears - 1
+      person.credits(i) = CInt(InputBox("Credits in year " & i + 1))
+    Next
+    DetermineStatus(person)
+  End Sub
+
+  Sub DetermineStatus(person As Student)
+    Dim query = From num In person.credits
+                Select num
+    Dim total As Integer = query.Sum
+    If (total >= 120) Then
+      txtResult.Text = person.name.firstName & " " &
+               person.name.lastName & " has enough credits to graduate."
+    Else
+      txtResult.Text = person.name.firstName & " " &
+                       person.name.lastName & " needs " &
+                       (120 - total) & " more credits to graduate."
+    End If
+  End Sub
+
+End Class
