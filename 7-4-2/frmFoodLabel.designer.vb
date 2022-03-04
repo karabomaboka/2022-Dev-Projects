441347d31493b5598466e5b281a19b96e7c6adc6
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-03-04 07:14:13 +0200

    Add files via upload

---
+++ 7-4-2/frmFoodLabel.designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFoodLabel
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Me.btnDetermine = New System.Windows.Forms.Button()
+    Me.dgvOutput = New System.Windows.Forms.DataGridView()
+    CType(Me.dgvOutput, System.ComponentModel.ISupportInitialize).BeginInit()
+    Me.SuspendLayout()
+    '
+    'btnDetermine
+    '
+    Me.btnDetermine.Location = New System.Drawing.Point(54, 10)
+    Me.btnDetermine.Margin = New System.Windows.Forms.Padding(4)
+    Me.btnDetermine.Name = "btnDetermine"
+    Me.btnDetermine.Size = New System.Drawing.Size(275, 36)
+    Me.btnDetermine.TabIndex = 0
+    Me.btnDetermine.Text = "Determine Nutritional Contents of a Meal"
+    Me.btnDetermine.UseVisualStyleBackColor = True
+    '
+    'dgvOutput
+    '
+    Me.dgvOutput.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+    Me.dgvOutput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+    Me.dgvOutput.Location = New System.Drawing.Point(51, 62)
+    Me.dgvOutput.Margin = New System.Windows.Forms.Padding(4)
+    Me.dgvOutput.Name = "dgvOutput"
+    Me.dgvOutput.RowHeadersVisible = False
+    Me.dgvOutput.Size = New System.Drawing.Size(282, 146)
+    Me.dgvOutput.TabIndex = 2
+    '
+    'frmFoodLabel
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(382, 224)
+    Me.Controls.Add(Me.dgvOutput)
+    Me.Controls.Add(Me.btnDetermine)
+    Me.Margin = New System.Windows.Forms.Padding(4)
+    Me.Name = "frmFoodLabel"
+    Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+    Me.Text = "Nutritional Analysis"
+    CType(Me.dgvOutput, System.ComponentModel.ISupportInitialize).EndInit()
+    Me.ResumeLayout(False)
+
+  End Sub
+  Friend WithEvents btnDetermine As System.Windows.Forms.Button
+  Friend WithEvents dgvOutput As System.Windows.Forms.DataGridView
+
+End Class
---
+++ 7-4-2/frmFoodLabel.vb
@@ -0,0 +1,65 @@
+﻿Public Class frmFoodLabel
+
+  Structure NutFact
+    Dim nutrient As String   'name of one of the five nutrients
+    Dim amount As Double     'amount of the nutrient in the meal
+  End Structure
+
+  Private Sub btnDetermine_Click(sender As System.Object, e As System.EventArgs) Handles btnDetermine.Click
+    Dim servings(9) As Double
+    Dim nutFacts(4) As NutFact           'This array of structures has an element
+    '                                     for each nutrient.
+    GetAmounts(servings)                 'input
+    nutFacts = ProcessData(servings)     'processing
+    ShowData(nutFacts)                   'output
+  End Sub
+
+  Sub GetAmounts(ByRef servings() As Double)
+    Dim foods() As String = IO.File.ReadAllLines("Foods.txt")
+    'Get the number of servings of each food.
+    For i As Integer = 0 To 9
+      servings(i) = CDbl(InputBox("How many servings of " & foods(i)))
+    Next
+  End Sub
+
+  Function ProcessData(servings() As Double) As NutFact()
+    Dim nutrients() As String = IO.File.ReadAllLines("Nutrients.txt")
+    Dim nutTable(,) As Double = {{23, 3, 0.3, 8100, 93},
+                                 {160, 2, 1, 9230, 46},
+                                 {230, 10, 3, 120, 343},
+                                 {85, 8, 0, 500, 302},
+                                 {65, 3, 1, 0, 24},
+                                 {178, 3.8, 0.9, 0, 18},
+                                 {110, 2, 1, 2510, 30},
+                                 {156, 2.4, 0.4, 7000, 80},
+                                 {575, 126.8, 3.6, 0, 73},
+                                 {405, 28.8, 26.6, 984, 190}}
+    Dim nutritionFacts(4) As NutFact  'This array of structures has an 
+    '                                  element for each nutrient.
+    For i As Integer = 0 To 4
+      nutritionFacts(i).nutrient = nutrients(i)  'Place the name of a nutrient
+      '                                           into an array element. 
+      'The next five lines calculate the total amount of the nutrient
+      'in the meal and place it into the array element.
+      Dim sum As Double = 0
+      For k As Integer = 0 To 9
+        sum += servings(k) * nutTable(k, i)
+      Next
+      nutritionFacts(i).amount = sum  'Place the amount of the nutrient into
+      '                                the array element.
+    Next
+    Return nutritionFacts
+  End Function
+
+  Sub ShowData(nutFacts() As NutFact)
+    'Create a query and use it to place the data from the array
+    'of structures into a DataGridView control.
+    Dim query = From element In nutFacts
+                Let Nutrient = element.nutrient
+                Let Amount = (element.amount).ToString("N")
+                Select Nutrient, Amount
+    dgvOutput.DataSource = query.ToList
+    dgvOutput.CurrentCell = Nothing
+  End Sub
+
+End Class
