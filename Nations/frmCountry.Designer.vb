f39f2f79b4ccad6939aeaec480722eeab224439f
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-03-06 21:15:53 +0200

    Add files via upload

---
+++ Nations/frmCountry.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCountry
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Me.components = New System.ComponentModel.Container()
+    Me.btnDisplay = New System.Windows.Forms.Button()
+    Me.lstCountries = New System.Windows.Forms.ListBox()
+    Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+    Me.SuspendLayout()
+    '
+    'btnDisplay
+    '
+    Me.btnDisplay.Location = New System.Drawing.Point(56, 12)
+    Me.btnDisplay.Name = "btnDisplay"
+    Me.btnDisplay.Size = New System.Drawing.Size(138, 62)
+    Me.btnDisplay.TabIndex = 0
+    Me.btnDisplay.Text = "Display Nations by Population"
+    Me.btnDisplay.UseVisualStyleBackColor = True
+    '
+    'lstCountries
+    '
+    Me.lstCountries.FormattingEnabled = True
+    Me.lstCountries.ItemHeight = 16
+    Me.lstCountries.Location = New System.Drawing.Point(56, 85)
+    Me.lstCountries.Name = "lstCountries"
+    Me.lstCountries.Size = New System.Drawing.Size(138, 132)
+    Me.lstCountries.TabIndex = 1
+    '
+    'frmCountry
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(250, 229)
+    Me.Controls.Add(Me.lstCountries)
+    Me.Controls.Add(Me.btnDisplay)
+    Me.Name = "frmCountry"
+    Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+    Me.Text = "U.N."
+    Me.ResumeLayout(False)
+
+  End Sub
+
+  Friend WithEvents btnDisplay As Button
+  Friend WithEvents lstCountries As ListBox
+  Friend WithEvents Timer1 As Timer
+End Class
---
+++ Nations/frmCountry.vb
@@ -0,0 +1,37 @@
+﻿Public Class frmCountry
+
+  Structure Nation
+    Dim name As String
+    Dim continent As String
+    Dim population As Double  'in millions
+    Dim area As Double        'in square miles
+  End Structure
+
+  Dim nations(192) As Nation
+
+  Private Sub frmCountry_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+    'Place the contents of UN.txt into the array nations
+    Dim line As String
+    Dim data() As String
+    Dim countries() As String = IO.File.ReadAllLines("UN.txt")
+    For i As Integer = 0 To 192
+      line = countries(i)
+      data = line.Split(","c)
+      nations(i).name = data(0)
+      nations(i).continent = data(1)
+      nations(i).population = CDbl(data(2))
+      nations(i).area = CDbl(data(3))
+    Next
+  End Sub
+
+  Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+    Dim query = From country In nations
+                Order By country.population Descending
+                Select country.name
+    lstCountries.Items.Clear()
+    For Each countryName As String In query
+      lstCountries.Items.Add(countryName)
+    Next
+  End Sub
+
+End Class
