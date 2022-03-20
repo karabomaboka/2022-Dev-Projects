22dfca01bcb4225277a87c47edd50de9e271cbff
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-03-20 18:22:58 +0200

    Add files via upload

---
+++ 3-2-1/frmOutput.designer.vb
@@ -0,0 +1,70 @@
+﻿Partial Public Class frmOutput
+  Inherits System.Windows.Forms.Form
+
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Public Sub New()
+    MyBase.New()
+
+    'This call is required by the Windows Form Designer.
+    InitializeComponent()
+
+  End Sub
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+    If disposing AndAlso components IsNot Nothing Then
+      components.Dispose()
+    End If
+    MyBase.Dispose(disposing)
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.ItemHeight = 16
+        Me.lstOutput.Location = New System.Drawing.Point(61, 16)
+        Me.lstOutput.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(159, 116)
+        Me.lstOutput.TabIndex = 0
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(61, 149)
+        Me.btnDisplay.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(160, 36)
+        Me.btnDisplay.TabIndex = 1
+        Me.btnDisplay.Text = "Display Output"
+        '
+        'frmOutput
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 197)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "frmOutput"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "3-2-1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lstOutput As System.Windows.Forms.ListBox
+  Friend WithEvents btnDisplay As System.Windows.Forms.Button
+
+End Class
---
+++ 3-2-1/frmOutput.vb
@@ -0,0 +1,11 @@
+﻿Public Class frmOutput
+
+  Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
+    Dim president As String
+    president = "George Washington"
+    lstOutput.Items.Clear()
+    lstOutput.Items.Add("president")
+    lstOutput.Items.Add(president)
+  End Sub
+
+End Class
