01423f76ebf0abdf13fe65127ed7e0cc26855186
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-03-31 21:17:59 +0200

    Add files via upload

---
+++ 3-2-5/frmEvaluate.designer.vb
@@ -0,0 +1,66 @@
+﻿Partial Public Class frmEvaluate
+  Inherits System.Windows.Forms.Form
+
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Public Sub New()
+    MyBase.New()
+
+    'This call is required by the Windows Form Designer.
+    InitializeComponent()
+
+  End Sub
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+    If disposing AndAlso components IsNot Nothing Then
+      components.Dispose()
+    End If
+    MyBase.Dispose(disposing)
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Me.lstResults = New System.Windows.Forms.ListBox
+    Me.btnEvaluate = New System.Windows.Forms.Button
+    Me.SuspendLayout()
+    '
+    'lstResults
+    '
+    Me.lstResults.FormattingEnabled = True
+    Me.lstResults.Location = New System.Drawing.Point(13, 13)
+    Me.lstResults.Name = "lstResults"
+    Me.lstResults.Size = New System.Drawing.Size(178, 95)
+    Me.lstResults.TabIndex = 0
+    '
+    'btnEvaluate
+    '
+    Me.btnEvaluate.Location = New System.Drawing.Point(13, 114)
+    Me.btnEvaluate.Name = "btnEvaluate"
+    Me.btnEvaluate.Size = New System.Drawing.Size(178, 31)
+    Me.btnEvaluate.TabIndex = 1
+    Me.btnEvaluate.Text = "Evaluate Expressions"
+    '
+    'frmEvaluate
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(206, 154)
+    Me.Controls.Add(Me.btnEvaluate)
+    Me.Controls.Add(Me.lstResults)
+    Me.Name = "frmEvaluate"
+    Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+    Me.Text = "3-2-5"
+    Me.ResumeLayout(False)
+
+  End Sub
+  Friend WithEvents lstResults As System.Windows.Forms.ListBox
+  Friend WithEvents btnEvaluate As System.Windows.Forms.Button
+
+End Class
---
+++ 3-2-5/frmEvaluate.vb
@@ -0,0 +1,18 @@
+﻿Public Class frmEvaluate
+
+  Private Sub btnEvaluate_Click(sender As System.Object, e As System.EventArgs) Handles btnEvaluate.Click
+    Dim str1, str2, str3 As String
+    str1 = "Quick as "
+    str2 = "a wink"
+    lstResults.Items.Clear()
+    lstResults.Items.Add(str1.Substring(0, 7))
+    lstResults.Items.Add(str1.IndexOf("c"))
+    lstResults.Items.Add(str1.Substring(0, 3))
+    lstResults.Items.Add((str1 & str2).Substring(6, 6))
+    lstResults.Items.Add((str1 & str2).ToUpper)
+    lstResults.Items.Add(str1.Trim & str2)
+    str3 = str2.Substring(str2.Length - 4)
+    lstResults.Items.Add("The average " & str3 & " lasts .1 second.")
+  End Sub
+
+End Class
