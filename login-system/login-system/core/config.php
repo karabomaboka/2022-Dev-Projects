019cf53b6c0f30812ecc7a6a28be2ac1b1626624
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-04-05 01:03:25 +0200

    Add files via upload

---
+++ login-system/login-system/core/config.php
@@ -0,0 +1,15 @@
+<?php 
+	define('DB_HOST', 'localhost');
+	define('DB_NAME', 'login');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
+	define('BASE_URL', 'http://localhost/login-system/');
+
+	//SMTP
+	define("M_HOST", 'smtp.gmail.com');
+	define("M_USERNAME", 'YourEmail@gmail.com');
+	define("M_PASSWORD", 'YourEmailPassword');
+	define("M_SMTPSECURE", 'ssl');
+	define("M_PORT", "465");
+	
+?>
